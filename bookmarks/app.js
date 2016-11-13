4cbe60e4de5bfc3af3c819c54d678ae14f880363
johnllao <john.lao@gmail.com>
2016-11-13 11:34:52 +0800

    add files

---
+++ bookmarks/app.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    var express = require('express');
+    var pug = require('pug');
+
+    var app = express();
+
+    app.set('views', './views');
+    app.set('view engine', 'pug');
+
+    app.use(express.static('./public'));
+
+    app.get('/', function (req, res) {
+        res.render('index');
+    });
+
+    app.get('/api/help/version', function (req, res) {
+        res.json({ version: '1.0.0' });
+    });
+
+    app.listen(8080);
+
+    console.log('started...');
+
+})();
